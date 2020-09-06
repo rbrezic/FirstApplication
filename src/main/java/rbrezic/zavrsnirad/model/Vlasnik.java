3004f79957171fb47ad0c1874053378bfd0b80c3
rbrezic <brezicrafael10@gmail.com>
2020-09-06 15:28:38 +0200

    ZavrsniRad01

---
+++ src/main/java/rbrezic/zavrsnirad/model/Vlasnik.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rbrezic.zavrsnirad.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Korisnik
+ */
+@Entity
+@Table(name="vlasnik")
+public class Vlasnik {
+   
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY) 
+    
+    private Long id;
+    
+    private String ime;
+    private String prezime;
+    private String kontakt;
+    private String oib;
+    private String spol;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getKontakt() {
+        return kontakt;
+    }
+
+    public void setKontakt(String kontakt) {
+        this.kontakt = kontakt;
+    }
+
+    public String getOib() {
+        return oib;
+    }
+
+    public void setOib(String oib) {
+        this.oib = oib;
+    }
+
+    public String getSpol() {
+        return spol;
+    }
+
+    public void setSpol(String spol) {
+        this.spol = spol;
+    }
+    
+   
+    
+    
+}
